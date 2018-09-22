f6ecd9de9e000f324875535b34edffbc8a950808
charlychiu <charlychiu@gmail.com>
2018-09-23 01:08:14 +0800

    xml-reader alpha edition

--- xml-reader.py
+++ xml-reader.py
@@ -6,45 +6,93 @@ def read_xml_file(file_path):
     if os.path.isfile(file_path):
         get_xml_tree = ET.parse(file_path)
         print("xml-reader: Detect root element tag: {}".format(get_xml_tree.getroot().tag))
-        return loading_from_xml(xml_tree=get_xml_tree)
+        return get_xml_tree
     else:
         print("xml-reader: Read file error (file not exist)")
         return
 
 
-def loading_from_xml(xml_tree):
+def full_text_search_in_xml(xml_tree, *args):
+    """
+        leave args empty -> retrieval all type
+        accept args : PubmedBookArticle, PubmedArticle
+    """
     documents = xml_tree.getroot()
-    count_of_document = len(documents)
     collection_list = []
     if ET.iselement(documents):
-        print("correctly set")
-        print(count_of_document)
-        document = documents.iter()
-        tmp_list = []
-        for child in document:
-            # child.tag child.attrib child.text
-            # if child.tag == "PubmedBookArticle" or child.tag == "PubmedBookArticle":
-            if len(tmp_list) == 2:
-                collection_list.append(tmp_list)
-                tmp_list = []
-            if child.tag == "ArticleTitle":
-                print("article title: {}".format(child.text))
-                tmp_list.append(child.text)
-            if child.tag == "AbstractText":
-                print("abstract text: {}".format(child.text))
-                tmp_list.append(child.text)
+        count_of_document = len(documents)
+        print("xml-reader: total documents: {}".format(str(count_of_document)))
+        print("xml-reader: correctly set")
+
+        if args:
+            for arg in args:
+                if arg == 'PubmedBookArticle':
+                    collection_list.extend(retrieval_pubmed_book_article(documents))
+                if arg == 'PubmedArticle':
+                    collection_list.extend(retrieval_pubmed_article(documents))
+                else:
+                    print('xml-reader: arg. error')
+        else:
+            collection_list.extend(retrieval_pubmed_book_article(documents))
+            collection_list.extend(retrieval_pubmed_article(documents))
+
         return collection_list
     else:
         print("setting fail")
         return list()
 
 
+def retrieval_pubmed_book_article(documents):
+    collection_list = []
+    ''' PubmedBookArticle type data'''
+    for child_of_root in documents.iterfind('PubmedBookArticle/BookDocument'):
+        tmp_list = []
+        # print(child_of_root.find('ArticleTitle').text)
+        tmp_list.append(child_of_root.find('ArticleTitle').text)  ## ArticleTitle
+        # print(child_of_root.find('Abstract/AbstractText'))
+        tmp_list.append(child_of_root.find('Abstract/AbstractText').text)  ## AbstractText
+        collection_list.append(tmp_list)
+
+    # for child_of_root in documents.iterfind('PubmedBookArticle/BookDocument/ArticleTitle'):
+    #     print("tag: {}, text: {}".format(child_of_root.tag, child_of_root.text))
+    # for child_of_root in documents.iterfind('PubmedBookArticle/BookDocument/Abstract/AbstractText'):
+    #     print("tag: {}, text: {}".format(child_of_root.tag, child_of_root.text))
+    return collection_list
+
+
+def retrieval_pubmed_article(documents):
+    collection_list = []
+    ''' PubmedArticle type data'''
+    for child_of_root in documents.iterfind('PubmedArticle/MedlineCitation/Article'):
+        tmp_list = []
+        # print(child_of_root.find('ArticleTitle').text)
+        tmp_list.append(child_of_root.find('ArticleTitle').text)  ## ArticleTitle
+        # print(child_of_root.findall('Abstract/AbstractText'))  ## Abstract
+
+        abstract_texts = child_of_root.findall('Abstract/AbstractText')
+        tmp_str = ''
+        for abstract_text in abstract_texts:
+            tmp_str += abstract_text.text + ' '
+        # print(tmp_str)
+        tmp_list.append(tmp_str)
+        collection_list.append(tmp_list)
+    return collection_list
+
+
+def pubmed_xml_parser(file_path, *args):
+    get_tree = read_xml_file(file_path)
+    parse_result = full_text_search_in_xml(get_tree, *args)
+    return parse_result
+
+
 if __name__ == "__main__":
     # For debug use
-    # print("error usage")
-    get_result = read_xml_file('./pubmed_result.xml')
+    # print("xml-reader: error usage")
+    # result = pubmed_xml_parser('./pubmed_result.xml', 'PubmedBookArticle')
+    result = pubmed_xml_parser('./pubmed_result.xml')
+
     print("!!")
-    print(len(get_result))
-    print(get_result[0])
-    print(get_result[1])
-    print(get_result[2])
+    print(len(result))
+    # print(get_result[0])
+    # print(get_result[1])
+    # print(get_result[2])
